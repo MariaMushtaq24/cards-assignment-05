5e21514aae0906804050ab8d523edee8323a1a72
MariaMushtaq24 <mariaashaikh25@gmail.com>
2024-12-13 18:29:25 +0500

    Add files via upload

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -10,7 +10,7 @@ return(
     width={120}
     height={120}/>
     
-    <div className="container justify-between w-full flex flex-row items-center bg-blue-600 h-20 ">
+    <div className="container justify-between w-full flex flex-row items-center bg-gray-400 h-20 ">
 <Link href="/Home" className="font-bold mx-16"> Home </Link>
 <Link href="/About" className="font-bold mx-16"> About </Link>
 <Link href="/Services" className="font-bold mx-16"> Services </Link>
@@ -19,12 +19,12 @@ return(
 <Link href="/Testimonials" className="font-bold mx-16"> Testimonials </Link>
     </div>
 <main>
-    <section className=" bg-black flex justify-center gap-5 h-auto w-full p-4 mt-8">
+    <section className=" bg-gray-400 flex justify-center gap-5 h-auto w-full p-4 mt-8">
         <div className="h-80 w-[450px] bg-blue-800 rounded-md shadow-2xl"></div>
         <div className="h-80 w-[450px] bg-blue-800 rounded-md shadow-2xl"></div>
         <div className="h-80 w-[450px] bg-blue-800 rounded-md shadow-2xl"></div>
     </section>
-    <section className=" bg-black flex justify-center gap-2 h-auto w-full p-4 mt-8">
+    <section className=" bg-gray-400 flex justify-center gap-2 h-auto w-full p-4 mt-8">
         <div className="h-80 w-[450px] bg-blue-800 rounded-md shadow-2xl"></div>
         <div className="h-80 w-[450px] bg-blue-800 rounded-md shadow-2xl"></div>
     </section>
